649dee3188ee92e7012caef9ccbacc9d5de2ab46
Erik Slagter <erikslagter@gmail.com>
2021-06-28 14:26:13 -0500

    Up to page 83

--- app.py
+++ app.py
@@ -13,3 +13,33 @@ def main():
     while not 1 <= shift <= 366:
         shift = int(input("Invalid value. Enter digit from 1 - 366: "))
     infile = input("Enter filename with extension: ")
+
+    if not os.path.exists(infile):
+        print("File {} not found. Terminiating.".format(infile), file=sys.stderr)
+        sys.exit(1)
+    text = load_file(infile)
+    char_dict = make_dict(text, shift)
+
+    if process == "encrypt":
+        ciphertext = encrypt(message, char_dict)
+        if check_for_fail(ciphertext):
+            print("\nProblem finding unique keys.", file=sys.stderr)
+            print("Try again, change message, or change code book.\n", file=sys.stderr)
+            sys.exit()
+        print("\nCharacter and number of occurances in char_dict: \n")
+        print("{:>10}{:>10}{:>10}".format("Character", "Unicode", "Count"))
+        for key in sorted(char_dict.keys()):
+            print(
+                "{:>10}{:>10}{:>10}".format(
+                    repr(key)[1:-1], str(ord(key)), len(char_dict[key])
+                )
+            )
+        print("\nNumber of distinct charcters: {}".format(ciphertext))
+        print("decrypted plaintext = ")
+
+        for i in ciphertext:
+            print(text[i - shift], end="", flush=True)
+
+    elif process == "decrypt":
+        plaintext = decrypt(message, text, shift)
+        print("\ndecrypted plaintext = \n {}".format(plaintext))
